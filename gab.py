bf571d4ece0d02e817606bd2305bdc564aba8079
RhodneyGypsmn <rhodneygypsmn@gmail.com>
2024-12-29 17:23:57 +0300

    That is my first code in python.

--- gab.py
+++ gab.py
@@ -1,2 +1,27 @@
 #This is my comment.
 print("Hello Rhodney Gypsmn this is really great work.")
+name = input("Enter your name: ")
+#Ask the user for their name.
+print(f"Hello,  {name}! Welcome to my Worklist")
+#Display the greeting.
+#Ask for the user's age.
+
+age = int(input("How old are you? "))
+#Calculate the year of birth.
+current_year = 2024
+birth_year = current_year - age
+print(f"Wow, {name}! You were born in {birth_year}.")
+
+#Ask for the user's favorite color.
+favorite_color = input("What is your favorite color? ")
+#Display a personalised message.
+print(f"Nice to meet you, {name}! You were born in {birth_year}, and your favorite color is {favorite_color}. ")
+print(f"Did you know {favorite_color} is a great color for creativity? Keep shining, {name}! ")
+#Ask for for user's hobby.
+hobby = input("What is your favorite hobby? ")
+#Display a personalised message.
+print(f"\nNice to meet you, {name}! ")
+print(f"You were born {birth_year}, and your favorite color is {favorite_color}.")
+print(f"It's great that you enjoy {hobby}. Doing what you love keeps happy and creative!")
+#Provide an inspirational message based on their hobby.
+print(f"Remember, {name}, every moment you spend on {hobby} is a step forwards mastering it. keep going!")
